fffac8b12e712bc9db701ee3ba98b40aeee43dfd
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-08-30 16:29:48 -0300

    Criação do enum OrderStatus para o pedido.

--- src/main/java/com/viniciusfinger/course/entities/Order.java
+++ src/main/java/com/viniciusfinger/course/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.viniciusfinger.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order") // adicionado para não confundir com o comando "Order" do SQL.
@@ -25,6 +26,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -33,9 +36,10 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
+		setOrderStatus(orderStatus);
 		this.moment = moment;
 		this.client = client;
 	}
@@ -62,6 +66,16 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		} 
 	}
 
 	@Override
