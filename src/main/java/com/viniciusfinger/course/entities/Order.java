d925ea42440068d75346c9044da8910f204e78ec
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-08-30 16:13:53 -0300

    Criação e aplicação do Order

--- src/main/java/com/viniciusfinger/course/entities/Order.java
+++ src/main/java/com/viniciusfinger/course/entities/Order.java
@@ -11,22 +11,33 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
-@Table(name = "tb_order") //adicionado para não confundir com o comando "Order" do SQL.
-public class Order implements Serializable{
+@Table(name = "tb_order") // adicionado para não confundir com o comando "Order" do SQL.
+public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1423659806458074162L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	public Order() {
-		
+
+	}
+
+	public Order(Long id, Instant moment, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.client = client;
 	}
 
 	public Long getId() {
