08d5b900a4f2a0abee6131824cd759e0d24d3e4c
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-09-07 15:30:17 -0300

    Criação do pagamento

--- src/main/java/com/viniciusfinger/course/config/TestConfig.java
+++ src/main/java/com/viniciusfinger/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.viniciusfinger.course.entities.Category;
 import com.viniciusfinger.course.entities.Order;
 import com.viniciusfinger.course.entities.OrderItem;
+import com.viniciusfinger.course.entities.Payment;
 import com.viniciusfinger.course.entities.Product;
 import com.viniciusfinger.course.entities.User;
 import com.viniciusfinger.course.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
 		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 } 
